efffc150a3f04d277f7318d39bce9b51d20b1f6a
Joseph-omoigui <Joseph-omoigui>
2023-02-17 12:23:11 -0800

    9-print_comd.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Prints all possible combinations of single-digit numbers.
  *
@@ -13,12 +14,12 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-
+		
 		putchar(',');
 		putchar(' ');
 	}
 
 	putchar('\n');
 
-	return (0)
+	return (0);
 }
