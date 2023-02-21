fd9abc37ec719943a004cfd000be2e4ddee80417
Joseph-omoigui <Joseph-omoigui>
2023-02-21 13:06:41 -0800

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,21 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Joseph Omoigui
+ */
+
 #include "main.h"
 /**
- * print_alphabet_x10 - prints the lowercase alphabet 10 times
- * Return: void
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
 void print_alphabet_x10(void)
 {
-int i, j;
-for (i = 0; i < 10; i++)
-for (j = 97; j <= 122; j++)
-_putchar(j);
-_putchar('\n');
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
