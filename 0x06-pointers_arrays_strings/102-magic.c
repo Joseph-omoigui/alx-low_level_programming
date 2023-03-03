4e105f0e878de97959d0f63e882df22774b54b71
Joseph-omoigui <Joseph-omoigui>
2023-03-03 03:24:33 -0800

    update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main(void)
 {
 int n;
@@ -8,7 +9,9 @@ a[2] = 1024;
 p = &n;
   /*
    * write your line of code here...
+   *
    * Remember:
+   *
    * - you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement
